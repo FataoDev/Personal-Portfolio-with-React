0db8c10eb22475f0c3cdf5dfbb80ecd6bdafb573
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-04-14 10:23:58 +0000

    Portfolio avec accueil et compétences uniquement

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Toggle from "../Toggle/Toggle";
+import "./Navbar.css";
+import { Link } from "react-scroll";
+const navbar = () => {
+  return (
+    <header className="n-wrapper" id="Navbar">
+      {/* left */}
+      <div className="n-left">
+        <div className="n-name">Fatao O</div>
+        <Toggle />
+      </div>
+      {/* right */}
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+            <li>
+              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true}>
+                Serivces
+              </Link>
+            </li>
+            <li>
+              <Link to="works" spy={true} smooth={true}>
+                Expériences
+              </Link>
+            </li>
+            <li>
+              <Link to="portfolio" spy={true} smooth={true}>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="testimonial" spy={true} smooth={true}>
+                Témoignage
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="contact" spy={true} smooth={true}>
+          <button className="button n-button">Contact</button>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default navbar;
