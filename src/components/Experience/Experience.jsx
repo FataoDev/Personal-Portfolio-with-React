a623fa86ede28c7f903231290377cf4d83e270c6
Fatao OUEDRAOGO <fataoouedraogo226@gmail.com>
2023-05-08 00:55:06 +0000

    fin responsive de la partie portfolio

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -23,7 +23,7 @@ const Experience = () => {
           className="circle"
           style={{ color: darkMode ? "var(--orange)" : "" }}
         >
-          7+
+          10+
         </div>
         <span style={{ color: darkMode ? "white" : "" }}>Projets </span>
         <span>Terminés</span>
